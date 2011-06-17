396279db54181bec2ae5c6820d60177edcb19616
Martyn <martynrdavies@gmail.com>
2011-06-17 16:25:32 +0100

    lots done

--- public/javascripts/trackchat.js
+++ public/javascripts/trackchat.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
 	$('.spinner').hide();
 	$('.errors').hide();
 	
-	$(".letsgo").click(function() {
+	function letsGo() {
 		var tweetStore = [];
 		
 		$('.button').fadeOut('slow');
@@ -13,13 +13,13 @@ $(document).ready(function(){
 		
 		$.getJSON('/tweets.json', function(twitter) {
 			$.each(twitter.data.tweets, function(i, tweet){
-				//$('.output-list').append('<li id='+i+'>'+ tweet.text + '</li>').fadeIn('slow');
-				//urlReadyText = escape(tweet.text);
 				urlReadyText = tweet.text
 				tweetStore.push(urlReadyText);
 			});
+			
 			var stripTweets = encodeURIComponent(tweetStore)
 			var url = 'http://developer.echonest.com/api/v4/artist/extract?api_key=TGUNRHOPCCFWRVO0A&format=jsonp&callback=?&text=' + stripTweets + '&results=35&sort=hotttnesss-desc&bucket=images';
+			
 			$.ajax({
 				cache: true,
 				dataType: "json",
@@ -28,7 +28,50 @@ $(document).ready(function(){
 				url: url,
 				success: function(echonest) {
 						$.each(echonest.response.artists, function(i, artist) {
-							$('.artist-list').append('<li class=\"'+i+'\"><a href=\"#\" class=\"info\" title=\"'+artist.id+'\"><img src=\"'+artist.images[0].url+'\" alt=\"'+ artist.name + '\" width=\"100\" height=\"100\" border=\"0\"/></a></li>');
+							var imgurl = [];
+							var imghref;
+							var blankimage = "/images/blank.gif";
+							if (artist.images[0] == undefined) {
+								imgurl.push(blankimage);
+							} else {
+							imgurl.push(artist.images[0].url);
+							};
+
+							// Check that the img is there, otherwise replace it with a holding image
+							$.ajax({
+						        type: "HEAD",
+						        url: imgurl,
+								crossDomain: false,
+								statusCode: {
+									301: function() {
+										imghref = "/images/blank.gif";
+									}
+								},
+						        success: function() {
+						        	imghref = imgurl;
+						        },
+								error: function() {
+									imghref = "/images/blank.gif";
+								} 
+						    });
+							
+							// if the imgurl is just empty then set it to a holding image
+							if (imgurl != null) {
+								imghref = imgurl;
+							} else {
+								imghref = "/images/blank.gif";
+							};
+							
+							// Append the image information and links to the <ul>
+							$('.artist-list').append('<li class=\"'+i+'\"><a href=\"#\" class=\"info\" title=\"'+artist.id+'\"><img src=\"'+imghref+'\" alt=\"'+ artist.name + '\" width=\"100\" height=\"100\" border=\"0\"/></a></li>');
+
+						});
+							
+						// Fade out the spinner, then fade in the artists
+						$('.spinner').fadeOut('slow', function() {
+							$('.artist-list').fadeIn('slow', function() {
+								$('.nav').fadeIn('slow');
+							});
 						});		
 				},
 				error: function(a, b, c) {
@@ -36,20 +79,56 @@ $(document).ready(function(){
 					//alert("Error: "+ a + b + c);
 				}
 			});
-			
-			// Fade out the spinner, then fade in the artists
-			$('.spinner').fadeOut('slow', function() {
-				$('.artist-list').fadeIn('slow');
-			});
-			
 		});
 	return false;
+	};
+	
+	// Go and get the artist information on click!
+	$('.info').live('click',function() {
+		$('#returned-info:visible').fadeOut('slow');
+		
+		var artistid = $(this).attr('title');
+		var artisturl = 'http://developer.echonest.com/api/v4/artist/profile?api_key=TGUNRHOPCCFWRVO0A&id='+artistid+'&bucket=id:7digital&bucket=urls&bucket=years_active&bucket=id:musicbrainz&format=jsonp&callback=?';
+		//alert(artistid);
+		$.ajax({
+			cache: true,
+			dataType: "json",
+			crossDomain: true,
+			jsonpCallback: "echonest",
+	        url: artisturl,
+	        success: function(echonest) {
+				var artistUrls = [];
+				var artistName = echonest.response.artist.name;
+				$.each(echonest.response.artist.urls, function(service, url) {
+					$('.listoflinks').append('<li><a href=\"'+url+'\">'+url+'</a></li>');
+				});
+				$('#returned-info .name .titlespan').after(artistName);
+				$('#returned-info').fadeIn('slow');
+	        },
+			error: function() {
+				$('.errors').append('<li>Sorry, there was an error getting information for that artist!</li>').fadeIn("slow");
+			} 
+	    });
+		$(this).children().css('border-color', 'red');
+		return false;
 	});
 	
-	$('.info').click(function() {
-		var artist_id = $(this).attr("title");
-		alert(artist_id);
-		return false;
+	// first big button
+	$(".letsgo").click(letsGo);
+	
+	$('.play').click(function() {
+		alert('play button clicked');
+	});
+		
+	// Reload function
+	$('.reload').click(function() {
+		$(this).html('Reloading...');
+		$('#returned-info:visible').fadeOut('slow');
+		$('.artist-list').fadeOut("slow", function() {
+			$('.artist-list li').delay(600).remove();
+			letsGo();
+			$('.reload').delay(600).html('Reload');
+		});	
 	});
 	
 });
